7dce2f75bb8cafb330f0f38af56334e94fe741de
Ruth <rwangare124@gmail.com>
2023-03-14 15:30:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const element = document.createElement("div");
+const main = document.getElementById('main');
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'My New Header';
+newHeader.id = 'victory';
+
+const ewHeader = document.createElement('h1');
+newHeader.textContent = `${'YOUR-NAME'} is the champion`;
+newHeader.id = 'victory';
